65505019c070d512392ac754f7b28364077b2918
xavierchenede <xavier.chenede@sanofi.com>
2023-02-17 14:26:37 +0000

    minor modif

--- programs/adadas.R
+++ programs/adadas.R
@@ -27,6 +27,51 @@ format_avisit <- function(x) {
   )
 }
 
+format_avisitn <- function(x) {
+  case_when(
+    x <=1             ~ 0,
+    x >=2  & x <= 84  ~ 8,
+    x >=85 & x <=140  ~ 16,
+    x >140            ~ 24
+  )
+}
+
+format_AWR <- function(x) {
+  case_when(
+    x <=1             ~ "<=1",
+    x >=2  & x <= 84  ~ "2-84",
+    x >=85 & x <=140  ~ "85-140",
+    x >140            ~ ">140"
+  )
+}
+
+format_AWT <- function(x) {
+  case_when(
+    x <=1             ~ 1,
+    x >=2  & x <= 84  ~ 56,
+    x >=85 & x <=140  ~ 112,
+    x >140            ~ 168
+  )
+}
+
+format_AWLO <- function(x) {
+  case_when(
+    x <=1             ~ NA_real_,
+    x >=2  & x <= 84  ~ 2,
+    x >=85 & x <=140  ~ 85,
+    x >140            ~ 141
+  )
+}
+
+format_AWHI <- function(x) {
+  case_when(
+    x <=1             ~ 1,
+    x >=2  & x <= 84  ~ 84,
+    x >=85 & x <=140  ~ 140,
+    x >140            ~ NA_real_
+  )
+}
+
 ##Look-up tables ----
 # Assign PARAMCD, PARAM, and PARAMN
 param_lookup <- tibble::tribble (
@@ -48,16 +93,14 @@ param_lookup <- tibble::tribble (
   "ACTOT"  ,"ACTOT"  ,"Adas-Cog(11) Subscore","15"
   )
 
-# Assign AVISIT, AVISITN, AWRANGE , AWTARGET
-range_lookup <- tibble::tribble (
-    ~AVISIT,   ~AVISITN, ~AWRANGE, ~AWTARGET, ~AWLO,         ~AWHI,          ~AWU,
-    "Baseline",0,      "<=1",      1,           NA_integer_,    1,            "DAYS",
-    "Week 8",  8,      "2-84",    56,          2,              84,           "DAYS",
-    "Week 16", 16,     "85-140",  112,         85,             140,          "DAYS",
-    "Week 24", 24,     ">140",    168,         141,           NA_integer_,   "DAYS"
-    )
-
-
+# locf for ACTOT records
+expected_ACTOT_records<- tibble::tribble (
+                          ~PARAMCD,   ~AVISIT,   ~AVISITN,
+                          "ACTOT",   "Baseline",   0,
+                          "ACTOT",   "Week 8",     8,
+                          "ACTOT",   "Week 16",    16,
+                          "ACTOT",   "Week 24",    24,
+)
 
 ##Derivations----
 ###Predecessors / TRTP(N)----
@@ -88,14 +131,14 @@ adadas <- adadas %>%
     check_type = "none",
     print_not_mapped = FALSE
   )
-  # Remoce unused QSTESTCD
-  adadas <- adadas %>% filter (QSTESTCD %in% unique((adadas$PARAMCD[!is.na(adadas$PARAMCD)])))
+  # Remove unused QSTESTCD
+adadas <- adadas %>% filter (QSTESTCD %in% unique((adadas$PARAMCD[!is.na(adadas$PARAMCD)])))
 
 
 ###AVAL / AVALC ----
 adadas <- adadas %>% mutate (AVAL= QSSTRESN, AVALC=QSSTRESC)
 
-## ABLFL / BASE /CHG / PCHG  ----
+### ABLFL / BASE / CHG / PCHG  ----
 adadas <- adadas %>% mutate(ABLFL=QSBLFL) %>%
                       derive_var_base(
                         by_vars = vars(STUDYID, USUBJID, PARAMCD),
@@ -106,23 +149,59 @@ adadas <- adadas %>% mutate(ABLFL=QSBLFL) %>%
                         derive_var_pchg()
 
 
-## AVISIT / AVISITN / AWRANGE / AWTARGET / AWU / AWLO /AWHI----
+### AVISIT / AVISITN ----
 adadas <- adadas %>%
-  mutate(AVISIT  = format_avisit(ADY))  %>%
-    ## from LOOK-UP table
-    derive_vars_merged_lookup(
-      dataset_add = range_lookup,
-      new_vars = vars (AVISITN, AWRANGE, AWTARGET, AWLO, AWHI, AWU),
-      by_vars = vars (AVISIT),
-      check_type = "none",
-      print_not_mapped = FALSE
-    ) %>%
-    mutate  (CHG=if_else(AVISIT != 'Baseline',CHG, NA_real_),
-            PCHG=if_else(AVISIT != 'Baseline',PCHG, NA_real_)
-    )%>%
-## AWTDIFF
-    mutate (AWTDIFF = abs(ADY-AWTARGET))
+  mutate(AVISIT   = format_avisit (ADY),
+         AVISITN  = format_avisitn(ADY),)
 
+### DTYPE----
+adadas <- adadas %>% restrict_derivation(
+    derivation = derive_locf_records,
+    args = params(
+      dataset_expected_obs = expected_ACTOT_records,
+      by_vars = vars(STUDYID, USUBJID, PARAMCD),
+      order = vars(AVISITN, AVISIT)
+    ),
+    filter = (PARAMCD=='ACTOT')
+  )
+
+  #complete LOCF records
+  # Retrieve LOCF data
+  date_actot <- adadas %>% select(USUBJID, AVISIT,AVISITN,ADT) %>%
+    arrange (USUBJID,AVISITN,ADT) %>%
+    distinct(USUBJID,AVISITN,AVISIT, ADT) %>%
+    mutate (ADT_LOCF = ADT, PARAMCD='ACTOT')
+
+  while(sum (is.na(date_actot$ADT_LOCF))>1){
+    date_actot<-date_actot %>%
+      mutate (ADT_LOCF= if_else (is.na(ADT_LOCF),lag(ADT_LOCF),ADT_LOCF))
+  }
+
+  date_actot <- date_actot %>% filter (is.na(ADT))
+
+  date_actot_formerg <- date_actot %>% select(-c(ADT)) %>% rename(ADT=ADT_LOCF)
+  locf_val <- adadas %>% select(-c(AVISIT, AVISITN))
+
+  locf_records <- inner_join (locf_val,date_actot_formerg, by=c("USUBJID", "ADT", "PARAMCD")) %>% mutate(DTYPE='LOCF')
+  adadas <- adadas %>% filter (is.na(DTYPE)) %>%
+                       bind_rows(locf_records) %>%
+                       mutate (ABLFL=(if_else (!is.na(DTYPE), NA_character_, ABLFL)))
+
+#update CHG and PCHG
+adadas <- adadas%>% mutate  (CHG=if_else(AVISIT != 'Baseline',CHG, NA_real_),
+            PCHG=if_else(AVISIT != 'Baseline',PCHG, NA_real_)
+            )
+
+###AWRANGE / AWTARGET / AWU / AWLO /AWHI----
+adadas <- adadas %>% mutate ( AWRANGE  = if_else(!is.na(DTYPE), format_AWR (AVISITN*7), format_AWR  (ADY)),
+                              AWTARGET = if_else(!is.na(DTYPE), format_AWT (AVISITN*7), format_AWT  (ADY)),
+                              AWLO     = if_else(!is.na(DTYPE), format_AWLO(AVISITN*7), format_AWLO (ADY)),
+                              AWHI     = if_else(!is.na(DTYPE), format_AWHI(AVISITN*7), format_AWHI (ADY)),
+                              AWU      = "DAYS"
+                            )
+
+## AWTDIFF
+adadas <- adadas %>% mutate (AWTDIFF = abs(ADY-AWTARGET))
 
 ## ANL01FL
 adadas <- adadas %>%
@@ -133,25 +212,10 @@ adadas <- adadas %>%
         mode = "first"
     )
 
-## DTYPE
-adadas <- adadas %>% mutate(DTYPE=(if_else(PARAMCD=="ACTOT", 'LOCF',"")))
-
-
-
-  # isol <- adadas %>% filter (USUBJID=="01-701-1294" & PARAMCD== "ACITM01")
-
-
-
-
-
-
-
-
 ## Final dataset----
 var_spec <- readxl::read_xlsx("./metadata/specs.xlsx", sheet = "Variables") %>%
-  dplyr::rename(type = "Data Type") %>%
-  rlang::set_names(tolower) %>% filter (dataset=="ADADAS")
-
+            dplyr::rename(type = "Data Type") %>%
+            rlang::set_names(tolower) %>% filter (dataset=="ADADAS")
 
 var_spec$type   <- recode(var_spec$type, text="Char")
 var_spec$type [str_ends(var_spec$variable, "DT")] <- "Date"
@@ -161,7 +225,7 @@ var_spec$order  <- as.numeric(var_spec$order)
 
 
 adadas <- adadas %>% select(var_spec$variable) %>%
-  arrange(USUBJID, PARAMCD, AVISIT, ADT)
+                     arrange(USUBJID, PARAMCD, AVISIT, ADT)
 
 ### Applying metadata----
 adadas <- adadas %>%
