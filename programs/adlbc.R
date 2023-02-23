710326f5f3ef95427a0fff2cd450c1218d59fd9c
xavierchenede <125265756+xavierchenede@users.noreply.github.com>
2023-02-23 11:44:43 +0100

    Update adlbc.R

--- programs/adlbc.R
+++ programs/adlbc.R
@@ -12,7 +12,7 @@ library(lubridate)
 library(stringr)
 
 ## Input datasets reading----
-lb <-  read_xpt ("./sdtm/lb.xpt")%>% filter(LBCAT=='CHEMISTRY')
+lb <-  read_xpt ("./sdtm/lb.xpt") %>% filter(LBCAT=='CHEMISTRY')
 adsl <- read_xpt ("./adam/adsl.xpt")
 
 ## Convert blanks to NA----
@@ -49,7 +49,7 @@ param_lookup <- tibble::tribble(
   "PROT",   "PROT",   "Protein (g/L)",                    32,
   "SODIUM", "SODIUM", "Sodium (mmol/L)",                  18,
   "URATE",  "URATE",  "Urate (umol/L)",                   28,
-  )
+)
 
 ##Derivations----
 ###Predecessors / TRTA(N) / TRTP(N)----
@@ -63,42 +63,39 @@ adlbc <- lb %>%
                      TRTEDT, TRTP=TRT01P, TRTPN=TRT01PN, TRTSDT, USUBJID, RFENDT),
     by_vars = vars(STUDYID, USUBJID)
   ) %>%
-
-### ADT / ADY ----
-   derive_vars_dt(new_vars_prefix = "A", dtc = LBDTC) %>%
-   derive_vars_dy(reference_date = TRTSDT, source_vars = vars(ADT))
+  ### ADT / ADY ----
+derive_vars_dt(new_vars_prefix = "A", dtc = LBDTC) %>%
+  derive_vars_dy(reference_date = TRTSDT, source_vars = vars(ADT))
 
 ###PARAM / PARAMCD / PARAMN----
 adlbc <- adlbc %>%
-## from LOOK-UP table
-    # Replace with PARAMCD lookup function
-    derive_vars_merged_lookup(
-      dataset_add = param_lookup,
-      new_vars = vars(PARAMCD, PARAM, PARAMN),
-      by_vars = vars(LBTESTCD),
-      check_type = "none",
-      print_not_mapped = FALSE
-    )%>%
-    ### PARCAT1 / AVAL / A1HI / A1HLO ----
-    mutate(
-      PARCAT1 = "CHEM",
-      AVAL = LBSTRESN,
-      A1LO = LBSTNRLO,
-      A1HI = LBSTNRHI,
-      ANRLO = LBSTNRLO,
-      ANRHI = LBSTNRHI,
-    ### R2A1LO  / R2A1HI ----
-     R2A1LO = AVAL / A1LO,
-     R2A1HI = AVAL / A1HI
-    )
-
-
+  ## from LOOK-UP table
+  # Replace with PARAMCD lookup function
+  derive_vars_merged_lookup(
+    dataset_add = param_lookup,
+    new_vars = vars(PARAMCD, PARAM, PARAMN),
+    by_vars = vars(LBTESTCD),
+    check_type = "none",
+    print_not_mapped = FALSE
+  )%>%
+  ### PARCAT1 / AVAL / A1HI / A1HLO ----
+mutate(
+  PARCAT1 = "CHEM",
+  AVAL = LBSTRESN,
+  A1LO = LBSTNRLO,
+  A1HI = LBSTNRHI,
+  ANRLO = LBSTNRLO*0.5,
+  ANRHI = LBSTNRHI*1.5,
+  ### R2A1LO  / R2A1HI ----
+  R2A1LO = AVAL / A1LO,
+  R2A1HI = AVAL / A1HI
+)
 
 ###ANRIND ----
 #requires the reference ranges ANRLO, ANRHI
 adlbc <- adlbc %>%
   derive_var_anrind() %>%
-  mutate(ANRIND = format_rind(ANRIND))
+  mutate(ANRIND = if_else( !is.na(ANRIND), format_rind(ANRIND),"N"))
 
 ### AVISIT / AVISITN----
 
@@ -117,6 +114,93 @@ adlbc <- adlbc %>%
       TRUE ~ as.numeric (str_remove(AVISIT,'Week '))
     )
   )
+
+###[PARAM / PARAMCD / PARAMN for changes]----
+adlbc_prev <- adlbc %>% mutate( ori_AVAL=AVAL,
+                                ori_PARAM=PARAM,
+                                ori_PARAMCD=PARAMCD,
+                                ori_PARAMN=PARAMN
+) %>%
+  select (-c(R2A1LO,R2A1HI, AVAL, starts_with("PARAM"))) %>%
+  group_by (USUBJID, ori_PARAMCD) %>%
+  arrange (USUBJID, ori_PARAMCD, AVISITN) %>%
+  mutate(AVAL    = round((ori_AVAL-lag(ori_AVAL))/(ANRHI-ANRLO),5),
+         PARAM   = paste0(ori_PARAM," change from previous visit, relative to normal range"),
+         PARAMCD = paste0("_",ori_PARAMCD),
+         PARAMN  = ori_PARAMN+100
+  ) %>%
+  ungroup() %>%
+  select(-c(starts_with("ori"), A1LO, A1HI,ANRLO , ANRHI ))
+
+adlbc <- bind_rows(adlbc_prev, adlbc)
+
+### ABLFL / BASE / BNRIND / CHG ----
+adlbc <- adlbc %>% restrict_derivation(
+  derivation = derive_var_extreme_flag,
+  args = params(
+    by_vars = vars(STUDYID, USUBJID, PARAMCD),
+    order = vars(ADT,VISITNUM),
+    new_var = ABLFL,
+    mode = "last"
+  ),
+  filter = (!is.na(AVAL) & ADT <= TRTSDT & VISITNUM==1)
+) %>%
+  derive_var_base(
+    by_vars = vars(STUDYID, USUBJID, PARAMCD),
+    source_var = AVAL,
+    new_var = BASE
+  ) %>%
+  derive_var_base(
+    by_vars = vars(STUDYID, USUBJID, PARAMCD),
+    source_var = ANRIND,
+    new_var = BNRIND
+  )
+
+
+adlbc <- adlbc %>% derive_var_chg()%>%
+  mutate (CHG=if_else(VISITNUM>1,CHG, NA_real_))
+
+
+### BR2A1LO / BR2A1HI----
+adlbc <-  adlbc %>% derive_var_base(
+  by_vars = vars(STUDYID, USUBJID, PARAMCD),
+  source_var = R2A1LO,
+  new_var = BR2A1LO
+) %>%
+  derive_var_base(
+    by_vars = vars(STUDYID, USUBJID, PARAMCD),
+    source_var = R2A1HI,
+    new_var = BR2A1HI
+  )
+
+
+### ALBTRVAL----
+# Maximum of [LBSTRESN-(1.5*ULN)] and [(.5*LLN) - LBSTRESN]
+# ==> pas clair les valeurs absolues _ mais ok avec compare!!!!
+
+adlbc <- adlbc %>%
+  mutate(ALBTRVAL_2= abs((0.5*LBSTNRLO)-LBSTRESN )) %>%
+  mutate(ALBTRVAL_1= abs(LBSTRESN - (1.5*LBSTNRHI))) %>%
+  rowwise() %>%
+  mutate(ALBTRVAL= max(c(ALBTRVAL_2,ALBTRVAL_1))) %>% select(-c(ALBTRVAL_1,ALBTRVAL_2))
+
+
+###ANL01FL----
+
+adlbc <- adlbc %>%
+  restrict_derivation(
+    derivation = derive_var_extreme_flag,
+    args = params(
+      by_vars = vars(STUDYID, USUBJID, PARAMCD),
+      order = vars(desc(ALBTRVAL), AVISITN, LBSEQ),
+      new_var = ANL01FL,
+      mode = "first",
+      check_type = "none"
+    ),
+    filter = !is.na(ALBTRVAL) & AVISITN > 0  & AVISITN <= 24
+  )
+
+
 #Retrieve EOT visit
 adlbc <- adlbc %>%
   derive_extreme_records(
@@ -128,136 +212,48 @@ adlbc <- adlbc %>%
       AVISIT = "End of Treatment",
       AVISITN = 99
     )) %>%
-    filter (!(AVISIT %in% c("Ambul Ecg Removal", "Retrieval")))
+  filter (!(AVISIT %in% c("Ambul Ecg Removal", "Retrieval")))
 
-
-
-###[PARAM / PARAMCD / PARAMN for changes]----
-adlbc_prev <- adlbc %>% mutate( ori_AVAL=AVAL,
-                                ori_PARAM=PARAM,
-                                ori_PARAMCD=PARAMCD,
-                                ori_PARAMN=PARAMN
-                                ) %>%
-  select (-c(R2A1LO,R2A1HI, AVAL, starts_with("PARAM"))) %>%
-  group_by (USUBJID, ori_PARAMCD) %>%
-  arrange (USUBJID, ori_PARAMCD, AVISITN) %>%
-  mutate(AVAL    = round((ori_AVAL-lag(ori_AVAL))/(ANRHI-ANRLO),5),
-         PARAM   = paste0(ori_PARAM," change from previous visit, relative to normal range"),
-         PARAMCD = paste0("_",ori_PARAMCD),
-         PARAMN  = ori_PARAMN+100
-         ) %>%
-         ungroup() %>%
-         select(-c(starts_with("ori"), A1LO, A1HI,ANRLO , ANRHI ))
-
-adlbc <- bind_rows(adlbc_prev, adlbc)
-
-
-
-
-### ABLFL / BASE / BNRIND / CHG ----
-adlbc <- adlbc %>% restrict_derivation(
-                  derivation = derive_var_extreme_flag,
-                  args = params(
-                    by_vars = vars(STUDYID, USUBJID, PARAMCD),
-                    order = vars(ADT,VISITNUM),
-                    new_var = ABLFL,
-                    mode = "last"
-                  ),
-                  filter = (!is.na(AVAL) & ADT <= TRTSDT & VISITNUM==1)
-                  ) %>%
-                  derive_var_base(
-                      by_vars = vars(STUDYID, USUBJID, PARAMCD),
-                      source_var = AVAL,
-                      new_var = BASE
-                  ) %>%
-                  derive_var_base(
-                    by_vars = vars(STUDYID, USUBJID, PARAMCD),
-                    source_var = ANRIND,
-                    new_var = BNRIND
-                  )
-
-
-adlbc <- adlbc %>% derive_var_chg()%>%
-  mutate (CHG=if_else(VISITNUM>1,CHG, NA_real_))
-
-
-### BR2A1LO / BR2A1HI----
-adlbc <-  adlbc %>% derive_var_base(
-          by_vars = vars(STUDYID, USUBJID, PARAMCD),
-          source_var = R2A1LO,
-          new_var = BR2A1LO
-        ) %>%
-          derive_var_base(
-            by_vars = vars(STUDYID, USUBJID, PARAMCD),
-            source_var = R2A1HI,
-            new_var = BR2A1HI
-        )
-
-
-### ALBTRVAL----
-# Maximum of [LBSTRESN-(1.5*ULN)] and [(.5*LLN) - LBSTRESN]
-# ==> pas clair les valeurs absolues _ mais ok avec compare!!!!
-
-adlbc <- adlbc %>%
-        mutate(ALBTRVAL_2= abs((0.5*LBSTNRLO)-LBSTRESN )) %>%
-        mutate(ALBTRVAL_1= abs(LBSTRESN - (1.5*LBSTNRHI))) %>%
-        rowwise() %>%
-        mutate(ALBTRVAL= max(c(ALBTRVAL_2,ALBTRVAL_1))) %>% select(-c(ALBTRVAL_1,ALBTRVAL_2))
 
 
 ### AENTMTFL----
 #Did not reach WEEk 24
 Last_obs1 <- adlbc %>% filter (COMP24FL == 'N' & !is.na(AVISIT)) %>%
-                        distinct(USUBJID,PARAMCD, VISITNUM)
+  distinct(USUBJID,PARAMCD, VISITNUM)
 
 #Completed WEEk 24 and performed visit 12
 Comp24_and_Vis12 <- adlbc %>% filter (COMP24FL == 'Y' & VISITNUM==12) %>%
-                    distinct(USUBJID,PARAMCD, VISITNUM) %>% mutate(C24_Vis12='Y')
+  distinct(USUBJID,PARAMCD, VISITNUM) %>% mutate(C24_Vis12='Y')
 
 #Completed WEEk 24 (even if visit 12 has not been performed)
 #in case visit 12 has not been performed, keep only the visit before visit 12
 Last_obs2 <- adlbc %>% filter (COMP24FL == 'Y' & !is.na(AVISIT))%>%
-                        distinct(USUBJID,PARAMCD, VISITNUM) %>%
-                        derive_vars_merged(
-                            dataset_add = Comp24_and_Vis12,
-                            new_vars = vars(C24_Vis12),
-                            by_vars = vars(USUBJID,PARAMCD)
-                        ) %>% filter(C24_Vis12=='Y' | (is.na(C24_Vis12) & VISITNUM < 12))
+  distinct(USUBJID,PARAMCD, VISITNUM) %>%
+  derive_vars_merged(
+    dataset_add = Comp24_and_Vis12,
+    new_vars = vars(C24_Vis12),
+    by_vars = vars(USUBJID,PARAMCD)
+  ) %>% filter(C24_Vis12=='Y' | (is.na(C24_Vis12) & VISITNUM < 12))
 
 #Put data together and retrieve last visit with treatment
 Last_obs <- bind_rows(Last_obs1, Last_obs2) %>%
-                    filter_extreme( by_vars  = vars(USUBJID, PARAMCD),
-                      order      = vars(VISITNUM),
-                      mode       = "last",
-                      check_type = "warning") %>%
-                      mutate(VISITNUM = if_else(VISITNUM>12, 12, VISITNUM))
+  filter_extreme( by_vars  = vars(USUBJID, PARAMCD),
+                  order      = vars(VISITNUM),
+                  mode       = "last",
+                  check_type = "warning") %>%
+  mutate(VISITNUM = if_else(VISITNUM>12, 12, VISITNUM))
 
 #Assign flag
 adlbc <- adlbc %>% restrict_derivation(
-                            derivation = derive_var_merged_exist_flag,
-                            args = params(
-                              dataset_add = Last_obs,
-                              by_vars = vars(USUBJID, PARAMCD,VISITNUM),
-                              new_var = AENTMTFL,
-                              condition = (!is.na(VISITNUM))
-                            ),
-                            filter = (!is.na(AVISIT) & AVISIT != "Baseline")
-                          )
-
-###ANL01FL [A CONTROLER] ----
-adlbc <- adlbc %>% mutate(ABS_CHG = abs(CHG)) %>%
-  restrict_derivation(
-    derivation = derive_var_extreme_flag,
-    args = params(
-      by_vars = vars(STUDYID, USUBJID, PARAMCD),
-      order = vars(ABS_CHG),
-      new_var = ANL01FL,
-      mode = "first",
-      check_type = "none"
-    ),
-    filter = !is.na(CHG) & AVISITN > 0
-  )
-
+  derivation = derive_var_merged_exist_flag,
+  args = params(
+    dataset_add = Last_obs,
+    by_vars = vars(USUBJID, PARAMCD,VISITNUM),
+    new_var = AENTMTFL,
+    condition = (!is.na(VISITNUM))
+  ),
+  filter = (!is.na(AVISIT) & AVISIT != "Baseline")
+)
 
 
 ## Final dataset----
