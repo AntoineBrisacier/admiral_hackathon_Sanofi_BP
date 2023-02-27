a64344ebc564be64304008d9f1a1bc9d23c387c3
AntoineBrisacier <antoine.brisacier@sanofi.com>
2023-02-27 08:33:03 +0000

    update token

--- install_git_script.R
+++ install_git_script.R
@@ -4,7 +4,7 @@ usethis::use_git_config(user.name = "AntoineBrisacier",
                         user.email = "antoine.brisacier@sanofi.com")
 usethis::use_git()
 usethis::create_github_token()
-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
+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
 
 gitcreds::gitcreds_set()
 usethis::use_github()
